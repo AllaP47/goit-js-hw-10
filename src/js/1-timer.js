4111675f7178725b11add890498a5fe8abc34aa8
AllaP47 <allapaschinska1@gmail.com>
2025-03-08 22:04:25 +0200

    add message

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,13 +3,12 @@ import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const datetimePicker = document.getElementById("datetime-picker");
+const datetimePicker = document.querySelector("#datetime-picker");
 const startButton = document.querySelector("[data-start]");
 const daysSpan = document.querySelector("[data-days]");
 const hoursSpan = document.querySelector("[data-hours]");
 const minutesSpan = document.querySelector("[data-minutes]");
 const secondsSpan = document.querySelector("[data-seconds]");
-
 let userSelectedDate = null;
 let timerInterval = null;
 
@@ -30,7 +29,6 @@ const options = {
     },
 };
 
-
 flatpickr(datetimePicker, options);
 
 function convertMs(ms) {
@@ -38,12 +36,12 @@ function convertMs(ms) {
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
-
+    
     const days = Math.floor(ms / day);
     const hours = Math.floor((ms % day) / hour);
     const minutes = Math.floor(((ms % day) % hour) / minute);
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
+    
     return { days, hours, minutes, seconds };
 }
 
@@ -61,15 +59,19 @@ function updateTimerDisplay({ days, hours, minutes, seconds }) {
 function startTimer() {
     startButton.disabled = true;
     datetimePicker.disabled = true;
+    
     timerInterval = setInterval(() => {
-        const timeLeft = userSelectedDate - new Date();
-        if (timeLeft <= 0) {
+        const now = new Date();
+        const remainingTime = userSelectedDate - now;
+        
+        if (remainingTime <= 0) {
             clearInterval(timerInterval);
             updateTimerDisplay({ days: 0, hours: 0, minutes: 0, seconds: 0 });
             datetimePicker.disabled = false;
             return;
         }
-        updateTimerDisplay(convertMs(timeLeft));
+        
+        updateTimerDisplay(convertMs(remainingTime));
     }, 1000);
 }
 
